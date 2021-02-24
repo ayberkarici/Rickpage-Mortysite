8df9fc7dbd231ff6a6b1f58205e3c8136868e85b
ayberkarici <ayberk816gg@gmail.com>
2021-02-24 21:12:15 +0300

    Random characters added.

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,13 @@ const sliderDiv = document.getElementById("slider-container");
 const miniCardsDiv = document.getElementById("mini-cards");
 const mainContainer = document.getElementById('main-container');
 const wrapper = document.getElementById('wrapp');
+const luckyContainer = document.getElementById('lucky_ten-container');
+const footerAuthor = document.getElementById('author-info');
 //const CHAR_API = "https://rickandmortyapi.com/api/character";
 //const LOCATION_API = "https://rickandmortyapi.com/api/location";
 //const EPISODE_API = "https://rickandmortyapi.com/api/episode"
 
-window.addEventListener("stalled", ()=> {
+mainContainer.addEventListener("load", ()=> {
    const loader = document.querySelector('loader') 
    const loaderGif = document.querySelector('loader-gif') 
 
@@ -36,7 +38,7 @@ async function getApi() {
 async function getFamily(member, card) {
     const roof = homeMember[card];
     const firstEpisode = await fetch(member.episode[0]);
-    const firsSeen = await firstEpisode.json();
+    const firstSeen = await firstEpisode.json();
 
     if (member.status == "Alive") {
         var a = "life-status-green"
@@ -47,23 +49,23 @@ async function getFamily(member, card) {
     roof.innerHTML += `
 
     <div class="family__container">    
-    <div class="member_img">
-        <img src="${member.image}" alt="${member.name}">
-    </div>
-    <div class="member_info">
-        <div class="member_div1">
-            <div class="member_name"><span>${member.name}</span></div>
-            <div class="member_status"><div class="${a}"></div> <span class="status-color">${member.status} - ${member.species}</span></div>
+        <div class="member_img">
+            <img src="${member.image}" alt="${member.name}">
         </div>
-        <div class="member_div2">
-            <div class="member_last-known-span"><span>Last known location:</span></div>
-            <div class="member_last-known-location"><span>${member.location.name}</span></div>
+        <div class="member_info">
+            <div class="member_div1">
+                <div class="member_name"><span>${member.name}</span></div>
+                <div class="member_status"><div class="${a}"></div> <span class="status-color">${member.status} - ${member.species}</span></div>
+            </div>
+            <div class="member_div2">
+                <div class="member_last-known-span"><span>Last known location:</span></div>
+                <div class="member_last-known-location"><span>${member.location.name}</span></div>
+            </div>
+            <div class="member_div3">
+                <div class="member_first-seen-span"><span>First seen in:</span></div>
+                <div class="member_first-seen-in"><span>${firstSeen.name}</span></div>
+            </div>
         </div>
-        <div class="member_div3">
-            <div class="member_first-seen-span"><span>First seen in:</span></div>
-            <div class="member_first-seen-in"><span>${firsSeen.name}</span></div>
-        </div>
-    </div>
     </div>    
     `
 }
@@ -81,14 +83,9 @@ async function getMiniCardsPage() {
         
         results.forEach(elem => {
             createMiniCards(elem);
-            getStatus(elem);
         });   
     }
-    xs++
-}
-
-function getStatus(elem) {
-
+    xs++;
 }
 
 function createMiniCards(elem) {
@@ -141,6 +138,8 @@ moreCardsBtn.onclick = ()=> {
     getMiniCardsPage();
 }
 
+
+// Load with scrolling 
 //const bodyELEM = document.getElementsByTagName("BODY")[0];
 //let xm = Number(1);
 //let xl = Number(1);
@@ -155,3 +154,75 @@ moreCardsBtn.onclick = ()=> {
 //        xl += 2
 //    }
 //})
+
+
+let numbers = []; // new empty array
+
+let min, max, r, n, p;
+
+min = 1;
+max = 662;
+r = 10; // how many numbers you want to extract
+
+for (let i = 0; i < r; i++) {
+  do {
+    n = Math.floor(Math.random() * (max - min + 1)) + min;
+    p = numbers.includes(n);
+    if(!p){
+      numbers.push(n);
+    }
+  }
+  while(p);
+}
+
+getRandomCards();
+
+async function getRandomCards() {
+    for (let i = 0; i < numbers.length; i++) {
+        const include = await fetch(API_URL + "character/" + Number(numbers[i]));
+        const info = await include.json();
+
+        createRandomCards(info);
+    }
+}
+
+async function createRandomCards(member) {
+    const firstEpisode = await fetch(member.episode[0]);
+    const firstSeen = await firstEpisode.json();
+
+    if (member.status == "Alive") {
+        var status = "mini-status-alive";
+    } else if (member.status == "Dead") {
+        var status = "mini-status-dead";
+    } else if (member.status == "unknown") {
+        var status = "mini-status-unknown";
+    } else {
+        var status = "mini-status-null";
+    }
+
+    luckyContainer.innerHTML += `
+    <div class="lucky_ten-display">
+        <div class="family__container">    
+            <div class="member_img">
+                <img src="${member.image}" alt="${member.name}">
+            </div>
+            <div class="member_info">
+                <div class="member_div1">
+                    <div class="member_name"><span>${member.name}</span></div>
+                    <div class="member_status"><div class="${status}"></div> <span class="status-color">${member.status} - ${member.species}</span></div>
+                </div>
+                <div class="member_div2">
+                    <div class="member_last-known-span"><span>Last known location:</span></div>
+                    <div class="member_last-known-location"><span>${member.location.name}</span></div>
+                </div>
+                <div class="member_div3">
+                    <div class="member_first-seen-span"><span>First seen in:</span></div>
+                    <div class="member_first-seen-in"><span>${firstSeen.name}</span></div>
+                </div>
+            </div>
+        </div>    
+    </div>
+    
+    `
+}
+
